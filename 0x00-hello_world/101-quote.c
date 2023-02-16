dfcb1bdaa85d7e0af3903ceabcd1cbedae06d601
Suralmk <surafelmelaku940@gmail.com>
2023-02-16 09:36:43 -0800

    wiseness

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,32 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * this_is_good_one_akx_team - function to use putchar
+ * @alx: parameter of the function
  *
+ * Description: this function is to print the statment without using
+ * printf na puts function.by using putchar  and putting all
+ * the elements of the alx array in a while loop is a way to print.
+ * the while loop terminates when the new line charachter is faced.
+ */
+
+void this_is_good_one_akx_team(char *alx)
+{
+int i = 0;
+while (alx[i] != '\n')
+{
+putchar(alx[i]);
+i++;
+}
+}
+
+/**
+ * main - Entry point
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-char *ch = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-putchar(ch);
-
+char *alx = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 return (0);
 }
