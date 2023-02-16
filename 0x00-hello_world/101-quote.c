bdfedbeca75d9e146f90bc4daf48110d339bc4de
Suralmk <surafelmelaku940@gmail.com>
2023-02-16 10:12:23 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,33 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
- * this_is_good_one_akx_team - function to use putchar
- * @alx: parameter of the function
- *
- * Description: this function is to print the statment without using
- * printf na puts function.by using putchar  and putting all
- * the elements of the alx array in a while loop is a way to print.
- * the while loop terminates when the new line charachter is faced.
- */
-
-void this_is_good_one_alx_team(char *alx)
-{
-int i = 0;
-while (alx[i] != '\n')
-{
-putchar(alx[i]);
-i++;
-}
-}
-
-/**
- * main - Entry point
- * Return: Always 0 (Success)
- */
+*main - Entry point
+*Return: Always 1
+*/
 
 int main(void)
 {
-char alx[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-this_is_good_one_alx_team(alx);
-return (0);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
 }
