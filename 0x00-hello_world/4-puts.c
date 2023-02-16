3ef038cdf32a007c5268ac8317b972e7825947e1
Suralmk <surafelmelaku940@gmail.com>
2023-02-16 02:52:54 -0800

    4-pts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 {
-	char *s = "\"Programming is like building a multilingual puzzle";
-	puts(s); 
-	return(0);
- }
+char *s = "\"Programming is like building a multilingual puzzle";
+puts(s);
+return (0);
+}
 
