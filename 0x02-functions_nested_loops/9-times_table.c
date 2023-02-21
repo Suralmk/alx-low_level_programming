b628db76a74e6ffec2e030cf7cfb964800faa1d2
Suralmk <surafelmelaku940@gmail.com>
2023-02-21 04:41:40 -0800

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * times_table - prints the nine time table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int k, i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j < 9; j++)
+		{
+		k = i * j;
+		if (k == 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(k + '0');
+		}
+		if (k <= 9 && k != 0)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(k + '0');
+		}
+		else if (k > 9)
+		{
+			_putchar(',');
+			_putchar(' ');
+			_putchar(k / 10 + '0');
+			_putchar(k % 10 + '0');
+		}
+		}
+	_putchar('\n');
+	}
+}
