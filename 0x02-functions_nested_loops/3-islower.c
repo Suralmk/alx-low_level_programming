475c82a609bf43f47b9f1151a66f7a93630bb2a8
Suralmk <surafelmelaku940@gmail.com>
2023-02-21 00:45:13 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower - cheaks for lowercase charachter
+ *
+ * Return: int
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+}
+}
