8eb3bfc27a8b826c35285added20a73404798753
Suralmk <surafelmelaku940@gmail.com>
2023-02-21 11:17:35 -0800

    add less than 1024

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * sum - compute the sum of multiples of 3 and 5
+ * below 1024*
+ */
+void sum(void)
+{
+	int sum3 = 0;
+
+	int sum5 = 0;
+
+	int tsum, n;
+
+for (n = 3; n < 10; n++)
+{
+if (n % 3 == 0)
+{
+sum3 += n;
+}
+if (n % 5 == 0)
+{
+sum5 += n;
+}
+	}
+tsum = sum3 + sum5;
+printf("%d\n", tsum);
+}
