db9a4b38c75eecc4e9ee27a0434675d6d241775c
Suralmk <surafelmelaku940@gmail.com>
2023-02-23 03:30:58 -0800

    new

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /*
- * _isupper - returns 1 if uppercase, else return 0
+ * _isupper - function return  uppercase, else return 0
+ *
  * @c: parameter of the function
  * Return: int
  */
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'Z')
-	       return (1);
-	else
+	if ((c >= 'A') && (c <= 'Z'))
+		return (1);
+	else if
 	return (0);
-}	
+}
