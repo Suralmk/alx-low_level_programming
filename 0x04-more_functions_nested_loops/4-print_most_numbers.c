7bbe3293b2a179a26dfa2f7d817f074ba08dce7c
Suralmk <surafelmelaku940@gmail.com>
2023-02-23 07:48:04 -0800

    most num

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_numbers - function to print numbers from 0 to 9
+ * print_most_numbers - function to print numbers from 0 to 9
  * Return: void
  */
 void print_most_numbers(void)
