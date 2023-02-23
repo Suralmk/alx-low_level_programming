636d9397d47b9b674f01c08f690bb47ca27d4716
Suralmk <surafelmelaku940@gmail.com>
2023-02-23 07:58:33 -0800

    line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
- * print_linr - prints a line to n
+ * print_line - prints a line to n
  * @n: the line is printed to numbe n
  */
 void print_line(int n)
 {
 	int i;
-	
+
 	for (i = 0; i < n; i++)
 	{
 		if (n <= 0)
+		{
 			break;
-		else
+		}
 			_putchar('_');
 	}
 	_putchar('\n');
