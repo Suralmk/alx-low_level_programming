154a1964ed96ec17bac7b4f03287f6b33525b3fb
Suralmk <surafelmelaku940@gmail.com>
2023-03-02 12:13:09 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,24 +8,11 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int j = 0;
-
-	while (s1[i] != '\0')
+while ((s1[i] != '\0') || (s2[i] != '\0'))
 	{
-		i++;
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+			i++;
 	}
-	while (s2[j] != '\0')
-	{
-		j++;
-	}
-	if (i > j)
-	{
-		return (i + j + 4);
-	}
-	else if (i < j)
-	{
-		return (-(i + j + 4));
-	}
-	else
-		return (0);
+	return (0);
 }
