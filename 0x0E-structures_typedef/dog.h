d9d7f8005b9b553dd861d00717d39477bec6461f
Suralmk <surafelmelaku940@gmail.com>
2023-03-21 12:06:11 -0700

    free

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
+
 /**
- * main - entry point
- * Return: Alwyas zero
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
  */
 struct dog
 {
-        char *name;
-        float age;
-        char *owner;
+char *name;
+float age;
+char *owner;
 };
 typedef struct dog dog_t;
 
