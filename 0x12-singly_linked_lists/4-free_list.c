ff69a8c477b39618688175c81df230df00bd5b19
Suralmk <surafelmelaku940@gmail.com>
2023-03-30 13:14:56 -0700

    free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_list - free the memory of list_t
+ * @head: head
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
+
