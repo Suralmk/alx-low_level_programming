85fdb1f032f43bf9d33ba71deb79f0849eec1717
Suramk <surafelmelaku940@gmail.com>
2023-07-06 08:53:09 -0700

    create hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,36 +3,34 @@
  * hash_table_create - Function that create a hash table
  * @size: The size of the array.
  *
- * Return: retturn null if any error happen. on success return  
+ * Return: retturn null if any error happen. on success return
  * pointer to the hast table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
 
-    hash_table_t *table;
-    unsigned long int idx = 0;
+hash_table_t *table;
+unsigned long int idx = 0;
 
-    table = malloc(sizeof(hash_node_t));
-    hash_node_t *size_of_array;
+table = malloc(sizeof(hash_node_t));
 
-    if (!table)
-    {
-        return NULL;
-    }
-    table->size = size;
-    table->array = malloc(sizeof(hash_node_t) * size);
+if (!table)
+{
+return (NULL);
+}
+table->size = size;
+table->array = malloc(sizeof(hash_node_t) * size);
 
-    if (table->array == NULL)
-    {
-        return NULL;
-    }
+if (table->array == NULL)
+{
+return (NULL);
+}
 
-    while (idx < size)
-    {
-        table->array[idx]= NULL;
+while (idx < size)
+{
+table->array[idx] = NULL;
 
-        idx++;
-    }
-    return (table);
- 
+idx++;
+}
+return (table);
 }
